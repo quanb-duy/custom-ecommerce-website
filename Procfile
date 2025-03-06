b819d012bde4757fb787cd2655d8e1ee293df603
quanb-duy <quanbuid@gmail.com>
2025-03-06 19:09:38 +0100

    dalsi fix railway

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
 web: node start.js
--- start.js
+++ start.js
@@ -1,6 +1,9 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const express = require('express');
-const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080;
 
